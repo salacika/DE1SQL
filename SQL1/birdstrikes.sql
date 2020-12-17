cc3788364df611053266c2111030ef49d9eb3938
Laszlo Sallo <salacika@gmail.com>
2020-12-17 03:05:19 +0100

    Update birdstrikes.sql

--- SQL1/birdstrikes.sql
+++ SQL1/birdstrikes.sql
@@ -15,6 +15,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+DROP SCHEMA IF EXISTS `birdstrikes`;
+CREATE SCHEMA  `birdstrikes`;
+USE `birdstrikes`;
+
 --
 -- Table structure for table `birdstrikes`
 --
